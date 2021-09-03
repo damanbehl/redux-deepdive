5d2e0faca30999564632cfb06bbe1981242edd94
Daman Behl <damanbehl@gmail.com>
2021-09-03 13:03:44 -0400

    api call in side-effect, taken out from reducer logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, Fragment } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+//define it outside of the component func so that this does not change and does not
+//re-initialize  if the component renders again this will be initialized when the file is parsed for the first time
+//so when the app first started
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.cart.showCart);
   const cart = useSelector((state) => state.cart);
-  const cartItems = cart.items;
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch("DUMMY URL", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "sending",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "DUMMY URL",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalItems: cart.totalItems,
+            totalPrice: cart.totalPrice,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("sending cart Data failed");
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "success",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "Sending cart data failed!",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart.items, cart.totalItems, cart.totalPrice, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products items={cartItems} />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products items={cart} />
+      </Layout>
+    </Fragment>
   );
 }
 
