bef0877422735e2ea30476485abe0d9ec23f6f8f
Daman Behl <damanbehl@gmail.com>
2021-09-03 01:22:58 -0400

    REDux and side effects - useEFFECT method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.cart.showCart);
-  const cartItems = useSelector((state) => state.cart.items);
+  const cart = useSelector((state) => state.cart);
+  const cartItems = cart.items;
+
+  useEffect(() => {
+    fetch("DUMMY URL", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
